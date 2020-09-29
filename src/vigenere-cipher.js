ba265f53a1fef1ddab1c6c0e9e52aab9e4da573d
bexon26 <47786103+bexon26@users.noreply.github.com>
2020-09-29 18:13:49 +0300

    add: vigenere task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,79 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(reverse = true) {
+    this.reverse = reverse;
+  }
+
+
+  encrypt(string, key) {
+    const arr_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let stringKeyWords = '';
+
+    let coutLetter = 0;
+
+    for (let i = 0; i < string.length; i++) {
+
+      if (arr_en.indexOf(string[i].toLowerCase()) == '-1') {
+        stringKeyWords += string[i];
+        coutLetter--;
+      } else {
+        if (arr_en.indexOf(string[i].toLowerCase()) + arr_en.indexOf(key[coutLetter].toLowerCase()) < arr_en.length) {
+          stringKeyWords += arr_en[arr_en.indexOf(string[i].toLowerCase()) + arr_en.indexOf(key[coutLetter].toLowerCase())];
+        } else {
+          stringKeyWords += arr_en[arr_en.indexOf(string[i].toLowerCase()) + arr_en.indexOf(key[coutLetter].toLowerCase()) - (arr_en.length)];
+        }
+      }
+
+
+
+      if (coutLetter < key.length - 1) {
+        coutLetter++;
+      } else {
+        coutLetter = 0;
+      }
+    }
+
+    if (this.reverse) {
+      return stringKeyWords.toUpperCase()
+    } else { return stringKeyWords.toUpperCase().split('').reverse().join('') }
+
+  }
+  decrypt(string, key) {
+    const arr_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let stringKeyWords = '';
+
+    let coutLetter = 0;
+
+
+    for (let i = 0; i < string.length; i++) {
+
+      if (arr_en.indexOf(string[i].toLowerCase()) == '-1') {
+        stringKeyWords += string[i];
+
+        coutLetter--;
+
+      } else {
+        if (arr_en.indexOf(string[i].toLowerCase()) - arr_en.indexOf(key[coutLetter].toLowerCase()) >= 0) {
+          stringKeyWords += arr_en[arr_en.indexOf(string[i].toLowerCase()) - arr_en.indexOf(key[coutLetter].toLowerCase())];
+        } else {
+          stringKeyWords += arr_en[arr_en.indexOf(string[i].toLowerCase()) - arr_en.indexOf(key[coutLetter].toLowerCase()) + (arr_en.length)];
+        }
+      }
+
+
+
+      if (coutLetter < key.length - 1) {
+        coutLetter++;
+      } else {
+        coutLetter = 0;
+      }
+    }
+
+    if (this.reverse) {
+      return stringKeyWords.toUpperCase()
+    } else { return stringKeyWords.toUpperCase().split('').reverse().join('') }
   }
 }
 
